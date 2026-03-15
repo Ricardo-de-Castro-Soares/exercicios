5474f969e0d7eef6ab2fd5352ae44d262da443d8
Ricardo-de-Castro-Soares <157640017+Ricardo-de-Castro-Soares@users.noreply.github.com>
2026-03-15 02:31:21 -0300

    Add files via upload

---
+++ Python/variavei constante e printando na tela em python/variaveis_constante.py
@@ -0,0 +1,5 @@
+nome, idade = ("Ricardo", 23)
+
+CIDADE_E_ESTADO_ONDE_MORO =  ["BA", "SALVADOR"]
+
+print("Olá, meu nome é", nome , "Tenho", idade , "Anos e moro em", CIDADE_E_ESTADO_ONDE_MORO, "e esse é meu primeiro codígo em Python")
