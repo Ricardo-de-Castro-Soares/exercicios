d8aff6868a3022014c6ad773ce6fe5d6442c51b8
Ricardo-de-Castro-Soares <157640017+Ricardo-de-Castro-Soares@users.noreply.github.com>
2026-03-15 02:51:42 -0300

    Add files via upload

---
+++ Python/variavei constante e printando na tela em python/variaveis_constante.py
@@ -0,0 +1,16 @@
+nome, idade = ("Ricardo", 23)
+
+CIDADE_E_ESTADO_ONDE_MORO =  ["BA", "SALVADOR"]
+
+print("Olá, meu nome é", nome , "Tenho", idade , "Anos e moro em", CIDADE_E_ESTADO_ONDE_MORO, "e esse é meu primeiro codígo em Python")
+
+"lorem agmagagamegoanepgnaginaignaignasignsnhsdtjdddddddddddddddddddjesjetkryktrltltltiltyiltiltu,u,ru,rtu,tu,tu,tu,tu,tu,tu,rtu,tu,tu,tu,tu,tu,t,tu,tu,tu,tu,tu,tu,t,tu,tukehwrhgsaeglorem agmagagamegoanepgnaginaignaignasignsnhsdtjdddddddddddddddddddjesjetkryktrltltltiltyiltiltu,u,ru,rtu,tu,tu,tu,tu,tu,tu,rtu,tu,tu,tu,tu,tu,t,tu,tu,tu,tu,tu,tu,t,tu,tukehwrhgsaeglorem agmagagamegoanepgnaginaignaignasignsnhsdtjdddddddddddddddddddjesjetkryktrltltltiltyiltiltu,u,ru,rtu,tu,tu,tu,tu,tu,tu,rtu,tu,tu,tu,tu,tu,t,tu,tu,tu,tu,tu,tu,t,tu,tukehwrhgsaeglorem agmagagamegoanepgnaginaignaignasignsnhsdtjdddddddddddddddddddjesjetkryktrltltltiltyiltiltu,u,ru,rtu,tu,tu,tu,tu,tu,tu,rtu,tu,tu,tu,tu,tu,t,tu,tu,tu,tu,tu,tu,t,tu,tukehwrhgsaeglorem agmagagamegoanepgnaginaignaignasignsnhsdtjdddddddddddddddddddjesjetkryktrltltltiltyiltiltu,u,ru,rtu,tu,tu,tu,tu,tu,tu,rtu,tu,tu,tu,tu,tu,t,tu,tu,tu,tu,tu,tu,t,tu,tukehwrhgsaeglorem agmagagamegoanepgnaginaignaignasignsnhsdtjdddddddddddddddddddjesjetkryktrltltltiltyiltiltu,u,ru,rtu,tu,tu,tu,tu,tu,tu,rtu,tu,tu,tu,tu,tu,t,tu,tu,tu,tu,tu,tu,t,tu,tukehwrhgsaeglorem agmagagamegoanepgnaginaignaignasignsnhsdtjdddddddddddddddddddjesjetkryktrltltltiltyiltiltu,u,ru,rtu,tu,tu,tu,tu,tu,tu,rtu,tu,tu,tu,tu,tu,t,tu,tu,tu,tu,tu,tu,t,tu,tukehwrhgsaeglorem agmagagamegoanepgnaginaignaignasignsnhsdtjdddddddddddddddddddjesjetkryktrltltltiltyiltiltu,u,ru,rtu,tu,tu,tu,tu,tu,tu,rtu,tu,tu,tu,tu,tu,t,tu,tu,tu,tu,tu,tu,t,tu,tukehwrhgsaeg"
+"gssssssssssssssssssssssssssss"
+"dddddddddddddddddddddddddddddddddddddddd"
+"ddddddddddddddddddddddddddddddddd"
+"gssssssssssssssssssssssssssss"
+"dddddddddddddddddddddddddddddddddddddddd"
+"ddddddddddddddddddddddddddddddddd"
+"gssssssssssssssssssssssssssss"
+"dddddddddddddddddddddddddddddddddddddddd"
+"ddddddddddddddddddddddddddddddddd"
