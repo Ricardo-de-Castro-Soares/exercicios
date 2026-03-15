013ea1cc4dbcfcda09223d5e72cab5597d5c2428
Ricardo-de-Castro-Soares <157640017+Ricardo-de-Castro-Soares@users.noreply.github.com>
2026-03-15 02:49:30 -0300

    Delete Python/variavei constante e printando na tela em python directory

--- Python/variavei constante e printando na tela em python/variaveis_constante.py
+++
@@ -1,5 +0,0 @@
-nome, idade = ("Ricardo", 23)
-
-CIDADE_E_ESTADO_ONDE_MORO =  ["BA", "SALVADOR"]
-
-print("Olá, meu nome é", nome , "Tenho", idade , "Anos e moro em", CIDADE_E_ESTADO_ONDE_MORO, "e esse é meu primeiro codígo em Python")
